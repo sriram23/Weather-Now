873b966ea65e9a424185c26cd7733818c40c265e
sriram23 <sriramsubramanian23@gmail.com>
2019-08-26 22:09:52 +0530

    Added Icons for weather

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 let key = "7727eb7a7ad3adf1d307938860eca01b"
 var cityName = "";
 var link = "";
+var time = new Date().getHours();
 function parseJson(){
 	cityName = document.getElementById('city').value;
 	link = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&apikey="+key;
@@ -14,8 +15,32 @@ function parseJson(){
 
 		if (request.status >= 200 && request.status < 400) {
 			document.getElementById('Temperature').innerHTML = "Temperature: "+obj.main.temp+"°C";
-			document.getElementById('Climate').innerHTML = "Climate: "+obj.weather[0].main;
+			document.getElementById('Climate').innerHTML = "Weather: "+obj.weather[0].main;
 			document.getElementById('City').innerHTML = obj.name;
+			if(obj.weather[0].main == "Haze"){
+				if(time<18 && time>6)
+					document.getElementById('image').src = "Resources/haze.png"
+				else
+					document.getElementById('image').src = "Resources/haze_night.png"
+			}
+			else if(obj.weather[0].main == "Clouds"){
+				document.getElementById('image').src = "Resources/cloud.png"	
+			}
+			else if(obj.weather[0].main == "Rain"){
+				document.getElementById('image').src = "Resources/rainy.png"	
+			}
+			else if(obj.weather[0].main == "Mist"){
+				document.getElementById('image').src = "Resources/misty.png"	
+			}
+			else if(obj.weather[0].main == "Clear"){
+				if(time<18 && time>6)
+					document.getElementById('image').src = "Resources/sumrise.png"
+				else
+					document.getElementById('image').src = "Resources/sunset.png"
+			}
+			else if(obj.weather[0].main == "Smoke"){
+				document.getElementById('image').src = "Resources/smoke.png"	
+			}
 		}
 		else{
 			document.getElementById('City').innerHTML = "The city doesn't exist! Kindly check";	
