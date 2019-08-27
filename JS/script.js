b6b777484c9837c1c45d05cff1c361f1cc457ef6
sriram23 <sriramsubramanian23@gmail.com>
2019-08-27 07:31:34 +0530

    Centered block, added flags

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 let key = "7727eb7a7ad3adf1d307938860eca01b"
 var cityName = "";
 var link = "";
+var flag_link = "";
 var time = new Date().getHours();
 function parseJson(){
 	cityName = document.getElementById('city').value;
@@ -17,6 +18,12 @@ function parseJson(){
 			document.getElementById('Temperature').innerHTML = "Temperature: "+obj.main.temp+"°C";
 			document.getElementById('Climate').innerHTML = "Weather: "+obj.weather[0].main;
 			document.getElementById('City').innerHTML = obj.name;
+			var country = obj.sys.country;
+			flag_link = "https://www.countryflags.io/"+country+"/shiny/64.png";
+			document.getElementById('flag').src = flag_link;
+
+
+			// document.getElementById('City1').innerHTML = obj.sys.country;
 			if(obj.weather[0].main == "Haze"){
 				if(time<18 && time>6)
 					document.getElementById('image').src = "Resources/haze.png"
