5d886f297ca11d0417fe132f6960e73ed9176e9d
sriram23 <sriramsubramanian23@gmail.com>
2019-08-31 15:24:44 +0530

    Custom fonts given, min max temp included

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 let key = "7727eb7a7ad3adf1d307938860eca01b"
 var cityName = "";
 var link = "";
+var link_trend = "";
 var flag_link = "";
 var time = new Date().getHours();
 
@@ -8,8 +9,6 @@ var time = new Date().getHours();
 function parseJson(){
 	cityName = document.getElementById('city').value;
 	link = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&apikey="+key;
-	console.log(link);
-	// location.reload();
 
 	var request = new XMLHttpRequest();
 	request.open('GET',link,true);
@@ -18,7 +17,7 @@ function parseJson(){
 		var obj = JSON.parse(this.response); 
 		if (request.status >= 200 && request.status < 400) {
 			var temp = Math.floor(obj.main.temp);
-			console.log(temp);
+			document.getElementById('Temperature').style.visibility = "visible";
 			if(temp<27)
 				document.getElementById('Temperature').className = "cool";
 			else if(temp>=27 && temp<30)
@@ -31,14 +30,15 @@ function parseJson(){
 				document.getElementById('Temperature').className = "vhot";
 
 			document.getElementById('Temperature').innerHTML = obj.main.temp+"°C";
-			document.getElementById('Climate').innerHTML = "Weather: "+obj.weather[0].main;
+			document.getElementById('Climate').innerHTML = "Weather: "+obj.weather[0].description;
 			document.getElementById('City').innerHTML = obj.name;
 			var country = obj.sys.country;
 			flag_link = "https://www.countryflags.io/"+country+"/shiny/64.png";
+			document.getElementById('flag').style.visibility = "visible";
 			document.getElementById('flag').src = flag_link;
 
 
-			// document.getElementById('City1').innerHTML = obj.sys.country;
+			document.getElementById('image').style.visibility = "visible";
 			if(obj.weather[0].main == "Haze"){
 				if(time<18 && time>6)
 					document.getElementById('image').src = "Resources/cloudy-day-1.svg"
@@ -49,7 +49,7 @@ function parseJson(){
 				document.getElementById('image').src = "Resources/cloudy.svg"	
 			}
 			else if(obj.weather[0].main == "Rain"){
-				document.getElementById('image').src = "Resources/rainy-4.svg"	
+				document.getElementById('image').src = "Resources/rainy-6.svg"	
 			}
 			else if(obj.weather[0].main == "Mist"){
 				document.getElementById('image').src = "Resources/snowy-4.svg"	
@@ -63,6 +63,11 @@ function parseJson(){
 			else if(obj.weather[0].main == "Smoke"){
 				document.getElementById('image').src = "Resources/snowy-6.svg"	
 			}
+
+			document.getElementById('temp_h').style.visibility = 'visible';
+			document.getElementById('temp_l').style.visibility = 'visible';
+			document.getElementById('htemp').innerHTML = obj.main.temp_min+"°C";
+			document.getElementById('ltemp').innerHTML = obj.main.temp_max+"°C";
 		}
 		else{
 			document.getElementById('City').innerHTML = "The city doesn't exist! Kindly check";	
@@ -72,6 +77,7 @@ function parseJson(){
 			document.getElementById('image').src = "";
 		}
 	}
-
 	request.send();
 }
+
+
